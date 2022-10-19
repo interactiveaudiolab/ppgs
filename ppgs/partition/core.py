8bd0988423fa8daf030de2a867afc13fabb8668d
CameronChurchwell <steamtrains@icloud.com>
2022-10-19 14:47:12 -0500

    Training and Validation Partition

--- ppgs/partition/core.py
+++ ppgs/partition/core.py
@@ -26,12 +26,13 @@ def datasets(datasets, overwrite=False):
             raise NotImplementedError
 
 #TODO check if SA1, SA2 should be filtered out
-def partition_dataset(data_dir, unseen_speakers, partition_file):
+def partition_dataset(data_dir, unseen_speakers, validation_files, partition_file):
     unseen_speakers = set(unseen_speakers)
     all_textgrid = list(data_dir.rglob('*.textgrid'))
-    train = (f'{file.parents[0].name}/{file.stem}' for file in all_textgrid if file.parents[0].name not in unseen_speakers)
-    test = (f'{file.parents[0].name}/{file.stem}' for file in all_textgrid if file.parents[0].name in unseen_speakers)
-    partition = {'train': list(train), 'test': list(test)}
+    valid = [f'{file.parents[0].name}/{file.stem}' for file in all_textgrid if file in validation_files]
+    train = [f'{file.parents[0].name}/{file.stem}' for file in all_textgrid if (file.parents[0].name not in unseen_speakers) and file not in validation_files]
+    test = [f'{file.parents[0].name}/{file.stem}' for file in all_textgrid if (file.parents[0].name in unseen_speakers) and file not in validation_files]
+    partition = {'train': train, 'valid': valid, 'test': test}
     partition_file.parents[0].mkdir(parents=True, exist_ok=True)
     with open(partition_file, 'w') as f:
         json.dump(partition, f, ensure_ascii=False, indent=4)
@@ -41,10 +42,22 @@ def arctic():
     data_dir = ppgs.CACHE_DIR / 'arctic'
     unseen_speakers = [f'cmu_us_{speaker}_arctic' for speaker in ppgs.ARCTIC_UNSEEN]
     partition_file = ppgs.PARTITION_DIR / 'arctic.json'
-    partition_dataset(data_dir, unseen_speakers, partition_file)
+    
+    #Get validation files
+    validation_files = []
+    for id in ppgs.ARCTIC_VALIDATION_IDS:
+        validation_files += [file for file in data_dir.rglob(f'{id}.textgrid') if file.parent.name[7:10] not in ppgs.ARCTIC_UNSEEN]
+
+    partition_dataset(data_dir, unseen_speakers, validation_files, partition_file)
 
 def timit():
     data_dir = ppgs.CACHE_DIR / 'timit'
     unseen_speakers = ppgs.TIMIT_UNSEEN
     partition_file = ppgs.PARTITION_DIR / 'timit.json'
-    partition_dataset(data_dir, unseen_speakers, partition_file)
+
+    #Get validation files
+    validation_files = []
+    for speaker in ppgs.TIMIT_VALID_SPEAKERS:
+        validation_files += list((data_dir / speaker).glob('*.textgrid'))
+
+    partition_dataset(data_dir, unseen_speakers, validation_files, partition_file)
