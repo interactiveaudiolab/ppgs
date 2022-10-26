ffe0a7e7635b4acfbee4842f5efc39ecde93e9d4
CameronChurchwell <steamtrains@icloud.com>
2022-10-26 11:17:01 -0500

    cleaned up w2v2 latent extraction

--- ppgs/preprocess/w2v2/core.py
+++ ppgs/preprocess/w2v2/core.py
@@ -9,13 +9,6 @@ import ppgs
 # Constants
 ###############################################################################
 
-#TODO reconfigure these
-# # PPG model checkpoint file
-# CHECKPOINT_FILE = ppgs.ASSETS_DIR / 'checkpoints' / 'ppg.pt'
-
-# # PPG model configuration
-# CONFIG_FILE = ppgs.ASSETS_DIR / 'configs' / 'ppg.yaml'
-
 # W2V2 pretrained model config name
 W2V2_CONFIG = "facebook/wav2vec2-base-960h"
 
@@ -33,7 +26,7 @@ def from_audio(
     sample_rate=ppgs.SAMPLE_RATE,
     config=W2V2_CONFIG,
     gpu=None):
-    """Compute PPGs from audio"""
+    """Compute W2V2 latents from audio"""
     device = torch.device('cpu' if gpu is None else f'cuda:{gpu}')
 
     # Cache model
@@ -48,27 +41,25 @@ def from_audio(
     # Setup features
     inputs = from_audio.processor(audio, sampling_rate=sample_rate, return_tensors='pt')
     inputs = inputs.to(device)
-    # length = torch.tensor([audio.shape[-1]], dtype=torch.long, device=device)
 
-    # Infer ppgs
+    # Infer W2V2 latents
     with torch.no_grad():
-        # return from_audio.model(audio, length)[0].T
         return from_audio.model(**inputs).last_hidden_state
 
 
 def from_file(audio_file, gpu=None):
-    """Compute PPGs from audio file"""
+    """Compute W2V2 latents from audio file"""
     return from_audio(ppgs.load.audio(audio_file), gpu=gpu).cpu()
 
 
 def from_file_to_file(audio_file, output_file, gpu=None):
-    """Compute PPGs from audio file and save to disk"""
+    """Compute W2V2 latents from audio file and save to disk"""
     ppg = from_file(audio_file, gpu)
     torch.save(ppg, output_file)
 
 
 def from_files_to_files(audio_files, output_files, gpu=None):
-    """Compute PPGs from audio files and save to disk"""
+    """Compute W2V2 latents from audio files and save to disk"""
     iterator = tqdm.tqdm(
         zip(audio_files, output_files),
         desc='Extracting W2V2 latents',
