fixed JSD calculation to ignore padding
Former-commit-id: 9dc28d9c360b554c533c07703bb87b1422133842

ppgs/evaluate/metrics.py
@@ -97,15 +97,25 @@ class JensenShannon:
 
     def update(self, predicted_logits, target_indices):
         """Update the total JSD"""
+        
+        #Unroll time dimensionality
         if len(predicted_logits.shape) == 3: #handle batched input
-            #Unroll time dimensionality
             predicted_logits = torch.transpose(predicted_logits, 1, 2) #Batch,Class,Time->Batch,Time,Class
             predicted_logits = torch.flatten(predicted_logits, 0, 1) #Batch,Time,Class->Batch*Time,Class
             target_indices = torch.flatten(target_indices) #Batch,Time->Batch*Time (1D)
+        
+        #deal with -100 ignore values
+        keep_indices = (target_indices != -100)
+        target_indices = target_indices[keep_indices]
+        predicted_logits = predicted_logits[keep_indices]
         target_onehot = torch.nn.functional.one_hot(target_indices, num_classes=predicted_logits.shape[-1])
-        # target_logits = torch.special.logit(target_onehot, eps=1e-6)
-        # self.total += jensenShannonDivergence(predicted_logits, target_logits)
-        # self.count += (target_indices != -100).sum() #TODO investigate if -100 needs to be used in JSD
+
+        #compute logits for targets
+        target_logits = torch.special.logit(target_onehot, eps=1e-6)
+
+        #calculate JSD and update totals
+        self.total += jensenShannonDivergence(predicted_logits, target_logits)
+        self.count += (target_indices != -100).sum() #TODO investigate if -100 needs to be used in JSD THE ANSWER IS YES!!
 
 ###############################################################################
 # Additional Metric Functions