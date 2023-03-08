3d1b4b2e0bd4609671cf8104d597ddd06437b438
CameronChurchwell <cameronchurchwell2024@u.northwestern.edu>
2023-03-08 04:29:43 +0000

    updated configs
    Former-commit-id: 6e797ab995e5c357073ddfebc42df7b60004ba63 [formerly 063d1b1e69151b7b00ce78025d6f1f11d0f3f1c2]
    Former-commit-id: 68b2d458515098f03b465f7f0624bef82e37ce46

--- ppgs/assets/configs/charsiubaseW2V2FS.py
+++ ppgs/assets/configs/charsiubaseW2V2FS.py
@@ -2,7 +2,7 @@ CONFIG = 'charsiubaseW2V2'
 MODULE = 'ppgs'
 
 INPUT_CHANNELS = 768 #dimensionality of wav2vec2 latents
-REPRESENTATION = 'w2v2'
+REPRESENTATION = 'w2v2fs'
 MODEL = 'convolution'
 NUM_WORKERS=2
 EVALUATION_BATCHES = 16
