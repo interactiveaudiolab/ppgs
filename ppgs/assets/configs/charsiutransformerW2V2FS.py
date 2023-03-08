063d1b1e69151b7b00ce78025d6f1f11d0f3f1c2
CameronChurchwell <cameronchurchwell2024@u.northwestern.edu>
2023-03-08 04:29:43 +0000

    updated configs

--- ppgs/assets/configs/charsiutransformerW2V2FS.py
+++ ppgs/assets/configs/charsiutransformerW2V2FS.py
@@ -2,8 +2,8 @@ CONFIG = 'charsiutransformerW2V2'
 MODULE = 'ppgs'
 
 INPUT_CHANNELS = 768 #dimensionality of wav2vec2 latents
-REPRESENTATION = 'w2v2'
+REPRESENTATION = 'w2v2fs'
 MODEL = 'transformer'
 NUM_WORKERS=2
 EVALUATION_BATCHES = 16
-NUM_STEPS = 2000000
+NUM_STEPS = 1000000
