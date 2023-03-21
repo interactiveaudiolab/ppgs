915015c4f565d5f2e1cf0d280a35b467c70d5a77
CameronChurchwell <cameronchurchwell2024@u.northwestern.edu>
2023-03-21 22:56:44 +0000

    disk usage measuring and w2v2fb

--- ppgs/model/wrapper.py
+++ ppgs/model/wrapper.py
@@ -3,13 +3,19 @@ import ppgs
 
 
 class WrapperModel(torch.nn.Module):
+    """Creates a wrapper module that acts like a sequential module but for submodules which take a different number of parameters each"""
 
     def __init__(self, modules, argListLengths=None):
+        """Initializes a WrapperModule with modules which take argListLengths number of arguments respectively
+        ex: [Transformer, Convolution], [2, 1]
+        in the case of a transformer which needs a mask
+        """
         super().__init__()
         self.module_list= torch.nn.ModuleList(modules)
         self.argListLengths = argListLengths
 
     def forward(self, *args):
+        """Forward call. Pass arguments for the first module in the ModuleList"""
         output = args
         if self.argListLengths:
             for module, numArgs in zip(self.module_list, self.argListLengths):
