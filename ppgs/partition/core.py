97d97a2a9843533c2ba3610e3531805f4148e45f
CameronChurchwell <cameronchurchwell2024@u.northwestern.edu>
2023-04-25 15:45:59 +0000

    changed partitions for arctic and timit

--- ppgs/partition/core.py
+++ ppgs/partition/core.py
@@ -4,7 +4,7 @@ import random
 import ppgs
 
 
-def datasets(datasets, overwrite=False):
+def datasets(datasets, overwrite=False, for_testing=False):
     """Partition datasets"""
     for dataset in datasets:
 
@@ -19,28 +19,32 @@ def datasets(datasets, overwrite=False):
         random.seed(ppgs.RANDOM_SEED)
 
         if dataset.lower() == 'arctic':
-            arctic()
+            arctic(for_testing)
         elif dataset.lower() == 'timit':
-            timit()
+            timit(for_testing)
         elif dataset.lower() == 'charsiu':
             charsiu()
         else:
             raise NotImplementedError
 
-#TODO check if SA1, SA2 should be filtered out
-def partition_dataset(data_dir, unseen_speakers, validation_files, partition_file):
+def partition_dataset(data_dir, unseen_speakers, validation_files, partition_file, for_testing=False):
     unseen_speakers = set(unseen_speakers)
     all_textgrid = list(data_dir.rglob('*.textgrid')) #TODO merge with wav rglob
-    valid = [f'{file.parents[0].name}/{file.stem}' for file in all_textgrid if file in validation_files]
-    train = [f'{file.parents[0].name}/{file.stem}' for file in all_textgrid if (file.parents[0].name not in unseen_speakers) and file not in validation_files]
-    test = [f'{file.parents[0].name}/{file.stem}' for file in all_textgrid if (file.parents[0].name in unseen_speakers) and file not in validation_files]
+    if for_testing:
+        test = [f'{file.parents[0].name}/{file.stem}' for file in all_textgrid]
+        train = []
+        valid = []
+    else:
+        valid = [f'{file.parents[0].name}/{file.stem}' for file in all_textgrid if file in validation_files]
+        train = [f'{file.parents[0].name}/{file.stem}' for file in all_textgrid if (file.parents[0].name not in unseen_speakers) and file not in validation_files]
+        test = [f'{file.parents[0].name}/{file.stem}' for file in all_textgrid if (file.parents[0].name in unseen_speakers) and file not in validation_files]
     partition = {'train': train, 'valid': valid, 'test': test}
     partition_file.parents[0].mkdir(parents=True, exist_ok=True)
     with open(partition_file, 'w') as f:
         json.dump(partition, f, ensure_ascii=False, indent=4)
 
 
-def arctic():
+def arctic(for_testing=False):
     data_dir = ppgs.CACHE_DIR / 'arctic'
     unseen_speakers = [f'cmu_us_{speaker}_arctic' for speaker in ppgs.ARCTIC_UNSEEN]
     partition_file = ppgs.PARTITION_DIR / 'arctic.json'
@@ -50,9 +54,9 @@ def arctic():
     for id in ppgs.ARCTIC_VALIDATION_IDS:
         validation_files += [file for file in data_dir.rglob(f'{id}.textgrid') if file.parent.name[7:10] not in ppgs.ARCTIC_UNSEEN]
 
-    partition_dataset(data_dir, unseen_speakers, validation_files, partition_file)
+    partition_dataset(data_dir, unseen_speakers, validation_files, partition_file, for_testing)
 
-def timit():
+def timit(for_testing=False):
     data_dir = ppgs.CACHE_DIR / 'timit'
     unseen_speakers = ppgs.TIMIT_UNSEEN
     partition_file = ppgs.PARTITION_DIR / 'timit.json'
@@ -62,13 +66,14 @@ def timit():
     for speaker in ppgs.TIMIT_VALID_SPEAKERS:
         validation_files += list((data_dir / speaker).glob('*.textgrid'))
 
-    partition_dataset(data_dir, unseen_speakers, validation_files, partition_file)
+    partition_dataset(data_dir, unseen_speakers, validation_files, partition_file, for_testing)
 
 def charsiu():
     """Partition dataset"""
     data_dir = ppgs.CACHE_DIR / 'charsiu'
     partition_file = ppgs.PARTITION_DIR / 'charsiu.json'
     stems = [file.stem for file in data_dir.glob('*.textgrid')]
+    stems = [stem for stem in stems if stem not in ppgs.CHARSIU_REJECT]
     # Get dataset stems
     random.seed(ppgs.RANDOM_SEED)
     random.shuffle(stems)
