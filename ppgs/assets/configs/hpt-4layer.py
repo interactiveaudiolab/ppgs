ac48279fdfba8ff811dd988f71b15ccc2751516f
CameronChurchwell <cameronchurchwell2024@u.northwestern.edu>
2023-06-13 16:50:17 +0000

    hyperparameter configs

---
+++ ppgs/assets/configs/hpt-4layer.py
@@ -0,0 +1,11 @@
+CONFIG = 'hpt-4layer'
+MODULE = 'ppgs'
+
+INPUT_CHANNELS = 768 #dimensionality of wav2vec2 latents
+REPRESENTATION = 'w2v2fb'
+MODEL = 'transformer'
+NUM_WORKERS=4
+EVALUATION_BATCHES = 16
+NUM_STEPS = 300000
+NUM_HIDDEN_LAYERS = 4
+MAX_FRAMES = 250000
