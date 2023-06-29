44b4d18a24e1bf010f77ef779e53f3843b6a6e70
CameronChurchwell <cameronchurchwell2024@u.northwestern.edu>
2023-06-29 17:15:41 +0000

    added encodec back to eval script

--- eval.sh
+++ eval.sh
@@ -3,4 +3,4 @@ python -m ppgs.evaluate --datasets timit arctic --checkpoint runs/w2v2fs/0020000
 python -m ppgs.evaluate --datasets timit arctic --checkpoint runs/w2v2fb/00200000.pt --config runs/w2v2fb/w2v2fb.py --gpu $1 --partition test
 python -m ppgs.evaluate --datasets timit arctic --checkpoint runs/mel/00200000.pt --config runs/mel/mel.py --gpu $1 --partition test
 python -m ppgs.evaluate --datasets timit arctic --checkpoint runs/unfold/00200000.pt --config runs/unfold/unfold.py --gpu $1 --partition test
-# python -m ppgs.evaluate --datasets timit arctic --checkpoint runs/encodec/00200000.pt --config runs/encodec/encodec.py --gpu $1 --partition test
+python -m ppgs.evaluate --datasets timit arctic --checkpoint runs/encodec/00200000.pt --config runs/encodec/encodec.py --gpu $1 --partition test
