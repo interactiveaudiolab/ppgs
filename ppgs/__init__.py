ctc fixes and masking improvements
Former-commit-id: 06c1a6238ffda7823d4fc63ed7270643e371b3bf [formerly 59a0f3e06155b4a9a7df264166de11ee7d96433f]
Former-commit-id: 160121ec7c48d6c7a26a171fa279c81e6ab0872c

ppgs/__init__.py
@@ -1,7 +1,7 @@
 # This line is necessary because the tensorboard devs have no idea how to code
 from torch.utils.tensorboard import SummaryWriter
 
-import faulthandler; faulthandler.enable()
+# import faulthandler; faulthandler.enable()
 
 ###############################################################################
 # Configuration

ppgs/model/transformer.py
@@ -2,11 +2,11 @@ import math
 import torch
 import ppgs
 
-def mask_from_lengths(lengths, offset=0):
+def mask_from_lengths(lengths, padding=0):
     """Create boolean mask from sequence lengths and offset to start. shape is batch x 1 x max_length"""
-    x = torch.arange(lengths.max()+offset, dtype=lengths.dtype, device=lengths.device)
+    x = torch.arange(lengths.max()+2*padding, dtype=lengths.dtype, device=lengths.device)
 
-    return ((x.unsqueeze(0)-offset < lengths.unsqueeze(1)) & (x.unsqueeze(0) >= offset)).unsqueeze(1)
+    return (x.unsqueeze(0)-2*padding < lengths.unsqueeze(1))
 
 ###############################################################################
 # Transformer stack