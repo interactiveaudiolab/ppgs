d979ed275fe83f0989324b8cee2e5754b9cd652b
Max Morrison <maxrmorrison@gmail.com>
2023-10-04 21:14:16 +0000

    Dataset statistics submodule
    Former-commit-id: 543cb9fe7a26d6eaf53c6b174673009017f713fb
    Former-commit-id: f0b13467c131934297651318d5a05a376483ffbc

---
+++ ppgs/data/stats/core.py
@@ -0,0 +1,39 @@
+import itertools
+
+import torchaudio
+
+import ppgs
+
+
+###############################################################################
+# Dataset statistics
+###############################################################################
+
+
+def process(datasets):
+    """Compute dataset statistics"""
+    for dataset in datasets:
+        directory = ppgs.CACHE_DIR / dataset
+
+        # Get stems
+        stems = list(
+            itertools.chain.from_iterable(
+                ppgs.load.partition(dataset).values()))
+
+        # Get duration in seconds
+        duration = 0.
+        for stem in ppgs.iterator(
+            stems,
+            f'Computing dataset statistics for {dataset}',
+            total=len(stems)
+        ):
+            info = torchaudio.info(directory / f'{stem}.wav')
+            duration += info.num_frames / info.sample_rate
+
+        # Convert to hours
+        duration /= 3600
+
+        # Report results
+        print(
+            f'The {dataset} dataset contains {duration:.2f} '
+            f'hours across {len(stems)} files')
