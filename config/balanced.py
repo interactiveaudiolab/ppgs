0c465ea50172236236dc259f9b1e9c27edac54f4
Max <maxrmorrison@gmail.com>
2024-01-13 16:35:07 -0600

    Update defaults; unused dependencies

--- config/balanced.py
+++ config/balanced.py
@@ -3,17 +3,5 @@ MODULE = 'ppgs'
 # Configuration name
 CONFIG = 'balanced'
 
-# Network width
-HIDDEN_CHANNELS = 512
-
-# Dimensionality of input representation
-INPUT_CHANNELS = 768
-
-# Number of hidden layers
-NUM_HIDDEN_LAYERS = 5
-
-# Input representation
-REPRESENTATION = 'w2v2fb'
-
 # Whether to use class-balanced loss weights
 CLASS_BALANCED = True
