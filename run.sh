4ee2aaecafe9ee68facefe4e06b1804f6a2ba400
Max <maxrmorrison@gmail.com>
2024-02-09 14:59:59 -0600

    Run and lint

--- run.sh
+++ run.sh
@@ -19,45 +19,41 @@ python -m ppgs.partition
 ###############################################################################
 
 
+# Run best model
+python -m ppgs.preprocess --gpu 0
+python -m ppgs.train --gpu 0
+python -m ppgs.evaluate --gpu 0
+
+
+###############################################################################
+# Run experiments
+###############################################################################
+
+
 # Wav2vec 2.0
 python -m ppgs.preprocess --representations w2v2fb --gpu 0
-accelerate launch -m ppgs.train --config config/w2v2fb.py
-python -m ppgs.evaluate \
-    --config config/w2v2fb.py \
-    --checkpoint runs/w2v2fb/00200000.pt \
-    --gpu 0
+python -m ppgs.train --config config/w2v2fb.py --gpu 0
+python -m ppgs.evaluate --config config/w2v2fb.py --gpu 0
 
 # Charsiu
 python -m ppgs.preprocess --representations w2v2fc --gpu 0
-accelerate launch -m ppgs.train --config config/w2v2fc.py
-python -m ppgs.evaluate \
-    --config config/w2v2fc.py \
-    --checkpoint runs/w2v2fc/00200000.pt \
-    --gpu 0
+python -m ppgs.train --config config/w2v2fc.py --gpu 0
+python -m ppgs.evaluate --config config/w2v2fc.py --gpu 0
 
 # Mel spectrogram
 python -m ppgs.preprocess --representations mel --gpu 0
-accelerate launch -m ppgs.train --config config/mel.py
-python -m ppgs.evaluate \
-    --config config/mel.py \
-    --checkpoint runs/mel/00200000.pt \
-    --gpu 0
+python -m ppgs.train --config config/mel.py --gpu 0
+python -m ppgs.evaluate --config config/mel.py --gpu 0
 
 # ASR bottleneck
 python -m ppgs.preprocess --representations bottleneck --gpu 0
-accelerate launch -m ppgs.train --config config/bottleneck.py
-python -m ppgs.evaluate \
-    --config config/bottleneck.py \
-    --checkpoint runs/bottleneck/00200000.pt \
-    --gpu 0
+python -m ppgs.train --config config/bottleneck.py --gpu 0
+python -m ppgs.evaluate --config config/bottleneck.py --gpu 0
 
 # EnCodec
 python -m ppgs.preprocess --representations encodec --gpu 0
-accelerate launch -m ppgs.train --config config/encodec.py
-python -m ppgs.evaluate \
-    --config config/encodec.py \
-    --checkpoint runs/encodec/00200000.pt \
-    --gpu 0
+python -m ppgs.train --config config/encodec.py --gpu 0
+python -m ppgs.evaluate --config config/encodec.py --gpu 0
 
 
 ###############################################################################
