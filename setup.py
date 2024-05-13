a15bc73042bee936b57afe341792fe063cc6dec6
Max Morrison <maxrmorrison@gmail.com>
2024-05-13 11:55:46 -0400

    Fixed setup dependencies

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md') as file:
 setup(
     name='ppgs',
     description='Phonetic posteriorgrams',
-    version='0.0.1',
+    version='0.0.2',
     author='Interactive Audio Lab',
     author_email='interactiveaudiolab@gmail.com',
     url='https://github.com/interactiveaudiolab/ppgs',
@@ -19,7 +19,6 @@ setup(
             'g2pM',
             'gdown>=4.6.2',
             'humanfriendly',
-            'librosa',
             'nltk',
             'pyyaml',
             'torch-complex',
@@ -28,6 +27,7 @@ setup(
     install_requires=[
         'espnet',
         'huggingface-hub',
+        'librosa',
         'moviepy',
         'numpy',
         'pypar',
